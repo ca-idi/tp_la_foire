7e6e6bc538f30d9db870ea6dd6b93fba36640754
ca-idi <casaidi94@gmail.com>
2025-11-09 14:59:05 +0100

    Modification de user.routes et du sous-dépôt tp_la_foire

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const User = require('../../../models/user.model');
+const User = require('../../projet/models/user.model');
 const router = express.Router();
 
 router.post('/add', async (req, res) => {
